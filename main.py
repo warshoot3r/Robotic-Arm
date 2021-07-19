4d775112f34715e48ce8394458af445e2401f0e5
james <james.huynh@mail.com>
2021-07-19 22:37:27 +0100

    Separating class files

--- main.py
+++ main.py
@@ -1,72 +1,6 @@
 import os
 import time
-class robot_arm:
-    def __init__(self, movement_name):
-        self.number_of_moves = 0
-        self.movement_name = movement_name
-        self.move_list = []
-    
-    def print_var(self):
-        print("printing the properties now")
-        print(
-            self.number_of_moves, self.movement_name
-          )
-        print("printing the move list")
-        
-    def add_moves(self, move_goal):
-        self.move_list.append([self.number_of_moves, move_goal])
-        self.number_of_moves += 1
-        #put the ROS states save here
-
-
-
-
-
-
-        #
-        print("added a move goal")
-
-    def print_moves(self):
-        for number_of_moves in self.move_list:
-            #Put the ROS move codes here
-            
-            
-
-
-            #
-            print(self.move_list[number_of_moves[0]] )
-            if(number_of_moves[0] == (self.move_list[-1][0]) ): 
-             print("printed all the move list now")    
-
-    def remove_last_move(self):
-            self.move_list.pop()
-            #print("removed array", self.move_list[array_number])
-            print("value of",self.move_list[-1][1],"was removed")
-
-    def write_to_file(self, saved_file_name):
-        __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-        full_saved_file_name = __location__ + "/" + saved_file_name + '.txt'
-        with open(full_saved_file_name, "w") as file:
-         for number_of_moves in self.move_list:
-            file.write( str(self.move_list[number_of_moves[0]][1]) )
-            file.write('\n')
-            if(number_of_moves[0] == (self.move_list[-1][0])):
-                print("Exported File")
-
-    def open_from_file(self, file_path):
-        __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-        file_location =  __location__ + "/" + file_path + ".txt"
-        with open(file_location, 'r') as opened_file:
-            print("Imported File")
-            print(opened_file.read())
-
-    def move_arm_routine(self, number_of_times):
-        print("Moving the routine name"+ ' ' + number_of_times + 'times')
-        for number in range(int(number_of_times)) :
-         for number_of_moves in self.move_list:
-          print(self.move_list[number_of_moves[0]])
-          if number_of_moves[0] == self.move_list[-1][0]:
-           print('\n')
+from robot_class import robot_arm
 
 def interactive_mode():
         print("#################################")
@@ -101,6 +35,7 @@ def interactive_mode():
                   interactive_mode_robot.open_from_file("save.robot")
         elif i == 'n' :
             print('Ok exiting...')
+            StopIteration
 
 
 
@@ -117,6 +52,9 @@ def play_mode(self):
     self.print_moves()
 
 
+
+""" 
+example commands.
 new = robot_arm("Move up")
 new.print_var()
 new.add_moves(30)
@@ -129,5 +67,5 @@ new.print_moves()
 new.write_to_file("new")
 new.open_from_file("new")
 new.move_arm_routine("3")
-
+ """
 interactive_mode()
